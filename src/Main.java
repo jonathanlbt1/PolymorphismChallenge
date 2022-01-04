d3afdac4a504ebf7aa141958800bdffd2c7f0956
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2022-01-04 12:29:49 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,124 @@
+class Car {
+    private String name;
+    private boolean engine;
+    private int cylinders;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.name = name;
+        this.engine = true;
+        this.cylinders = cylinders;
+        this.wheels = 4;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+}
+
+class Opala extends Car {
+
+    public Opala(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() +  " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
+
+class MobiLike extends Car {
+
+    public MobiLike(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() +  " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
+
+class Mitsubishi extends Car {
+
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+        System.out.println();
+
+        Opala opala = new Opala(6, "Commodore");
+        System.out.println(opala.startEngine());
+        System.out.println(opala.accelerate());
+        System.out.println(opala.brake());
+        System.out.println();
+
+        MobiLike mobilike = new MobiLike(6, "Like Bosta");
+        System.out.println(mobilike.startEngine());
+        System.out.println(mobilike.accelerate());
+        System.out.println(mobilike.brake());
+
+    }
+}
